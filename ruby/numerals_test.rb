c064a2f082981f61c33625cdc227b23aa192222b
Anton Zhiliuk <crashtown.pal@gmail.com>
2016-07-15 14:11:37 +0300

    add test for transformin thousand node to words

--- ruby/numerals_test.rb
+++ ruby/numerals_test.rb
@@ -2,8 +2,6 @@ require "./numerals.rb"
 require "minitest/autorun"
 
 class NumeralsTest < Minitest::Test
-  def setup
-  end
 
   def test_parses_number_by_thousands
     assert_equal [[0, 0, 7],
@@ -29,5 +27,16 @@ class NumeralsTest < Minitest::Test
                   :empty,
                   [3, 2, 1]],
                  to_abstract_tree(5_437_000_321)
+  end
+
+  def test_transform_thousand_node_to_words
+    assert_equal "six hundred and two", thousand_node_to_words([6, 0, 2])
+    assert_equal "thirty three", thousand_node_to_words([0, 3, 3])
+    assert_equal "one", thousand_node_to_words([0, 0, 1])
+  end
+
+  def test_transform_teens
+    assert_equal "eighteen", thousand_node_to_words([0, 1, 8])
+    assert_equal "sixteen", thousand_node_to_words([0, 1, 6])
   end
 end
